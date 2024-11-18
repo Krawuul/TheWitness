ba84074d2b8f10a0e5677b3046fa83c249be1b4f
Maxime Chauvin <134788464+Sobriket@users.noreply.github.com>
2024-11-18 12:50:25 +0100

    Schedule reader + Resources folder added

---
+++ Dev/TheWitness/Assets/Scripts/NPCs/ScheduleManager.cs
@@ -0,0 +1,110 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class ScheduleManager : MonoBehaviour
+{
+    public enum DAYS
+    {
+        MONDAY,
+        TUESDAY,
+        WEDNESDAY,
+        THURSDAY,
+        FRIDAY,
+        SATURDAY,
+        SUNDAY,
+    };
+
+    public enum TIMESTEP
+    {
+        MORNING,
+        NOON,
+        AFTER_NOON,
+        EVENING
+    };
+
+    [SerializeField] string filepath;
+    List<string>[,] list = new List<string>[7, 4];
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if(filepath.NullIfEmpty() == null)
+        {
+            filepath = "Assets\\Resources\\NPCsSchedule.csv";
+        }
+        string[] read;
+
+        
+        using (StreamReader sr = new StreamReader(filepath))
+        {
+            string dataReceived;
+            int id2 = 0;
+            dataReceived = sr.ReadLine();
+            while ((dataReceived = sr.ReadLine()) != null)
+            {
+                read = dataReceived.Split(',', StringSplitOptions.RemoveEmptyEntries);
+                List<string> listNpc = new List<string>();
+                int id1 = 0;
+                for (int i = 1; i < read.Length; i++)
+                {
+                    if (read[i].First() == '"')
+                    {
+                        listNpc.Add(read[i].Substring(1));
+                    }
+                    else
+                    {
+                        if (read[i].Last() == '"')
+                        {
+                            listNpc.Add(read[i].Substring(0, read[i].Length - 1));
+                            string[] tArray = new string[listNpc.Count];
+                            listNpc.CopyTo(tArray);
+                            list[id1, id2] = tArray.ToList();
+
+                            listNpc.Clear();
+                            id1++;
+                        }
+                        else
+                        {
+                            listNpc.Add(read[i]);
+                        }
+                    }
+                }
+
+
+                id1 = 0;
+                id2++;
+            }
+        }
+
+        Debug.Log(ToString());
+    }
+
+    public override string ToString()
+    {
+        string text = string.Empty;
+        for (int i = 0; i < list.GetLength(0); i++)
+        {
+            for (int j = 0; j < list.GetLength(1); j++)
+            {
+                text += ((DAYS)i).ToString() + " " + ((TIMESTEP)j).ToString() + " :" + "\n";
+                foreach (string s in list[i, j])
+                {
+                    text += "\t" + s;
+                }
+                text += "\n";
+            }
+
+        }
+        return text;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
