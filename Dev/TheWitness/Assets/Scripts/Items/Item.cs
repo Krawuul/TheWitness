c344397f80f6dff8340e47b7e4ec4bf96a01b4be
Crepiliere <acrepilierepokemon@gmail.com>
2024-11-30 14:15:28 +0100

    Update Item.cs
    Suppression des using inutile qui empeche la compile

--- Dev/TheWitness/Assets/Scripts/Items/Item.cs
+++ Dev/TheWitness/Assets/Scripts/Items/Item.cs
@@ -1,8 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using static UnityEditor.FilePathAttribute;
-using UnityEngine.Windows;
 
 public class Item : MonoBehaviour, ICollectable
 {
